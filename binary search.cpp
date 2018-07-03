b4ab57b7019634277d0d2aed950f070c26d7fd44
Rakibul39 <36725046+Rakibul39@users.noreply.github.com>
2018-07-02 22:19:17 -0930

    Add files via upload

---
+++ binary search.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a[20];
+    int n, i, beg, en, mid,item;
+    cout<<"enter number of elements"<<endl;
+    cin>>n;
+
+    cout<<"enter your elements"<<endl;
+    for(i=0;i<n;i++){
+        cin>>a[i];
+    }
+    cout<<"your elements is:"<<endl;
+    for(i=0;i<n;i++){
+        cout<<a[i]<<" ";
+    }
+    cout<<"\nenter item to search"<<endl;
+    cin>>item;
+    beg=1;
+    en=n;
+    mid=(beg+en)/2;
+    while(beg<=en && a[mid]!=item){
+        if(a[mid]<item)
+            beg=mid+1;
+    else
+        en= mid-1;
+    }
+    if(a[mid]==item){
+        cout<<"\n item found at location\t" <<mid;
+       }
+
+    else
+        cout<<"\n number not found";
+
+
+    return 0;
+}
